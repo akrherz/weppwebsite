df1a11cf02ea78428f43c91eb8d14041a0771705
akrherz <akrherz@iastate.edu>
2013-06-04 19:43:41 -0500

    cleanups

--- scripts/GIS/monthlyPrecip.py
+++ scripts/GIS/monthlyPrecip.py
@@ -1,65 +1,63 @@
-#!/usr/bin/env python
-
-import pg
+"""
+  Generate a monthly shapefile of precipitation data
+"""
+import psycopg2
 import dbflib
-import mx.DateTime
+import datetime
 import shutil
 import os
 import sys
-mydb = pg.connect('wepp','iemdb')
+WEPP = psycopg2.connect(database='wepp', host='iemdb', user='nobody')
+wcursor = WEPP.cursor()
 
 if len(sys.argv) == 1:
-    now = mx.DateTime.now() - mx.DateTime.RelativeDateTime(days=1)
-    sts = now + mx.DateTime.RelativeDateTime(day=1)
-    ets = sts + mx.DateTime.RelativeDateTime(months=1)
+    now = datetime.datetime.now() - datetime.timedelta(days=1)
+    sts = now.replace(day=1)
+    ets = (sts + datetime.timedelta(days=35)).replace(day=1)
 else:
-    sts = mx.DateTime.DateTime(int(sys.argv[1]),1,1)
-    ets = mx.DateTime.DateTime(int(sys.argv[1]),12,2)
+    sts = datetime.datetime(int(sys.argv[1]),1,1)
+    ets = datetime.datetime(int(sys.argv[1]),12,31)
 
-interval = mx.DateTime.RelativeDateTime(months=+1)
 
 now = sts
 ohrap = {}
-rs = mydb.query("SELECT hrap_i from hrap_utm ORDER by hrap_i ASC").dictresult()
-for i in range(len(rs)):
-    ohrap[ int(rs[i]['hrap_i']) ] = {'rain': 0, 'hours': 0, 'mrain': 0}
+wcursor.execute("SELECT hrap_i from hrap_utm ORDER by hrap_i ASC")
+for row in wcursor:
+    ohrap[ row[0] ] = {'rain': 0, 'hours': 0, 'mrain': 0}
 
 hrapi = ohrap.keys()
 hrapi.sort()
 
-while (now < ets):
-    print "Hello Heather, I am here ", now
-    dbfname = "monthlyrain/%s_rain" % (now.strftime("%Y%m"), )
+while now < ets:
+    print "monthlyPrecip processing:", now.strftime("%b %Y")
+    dbfname = "%s_rain" % (now.strftime("%Y%m"), )
     dbf = dbflib.create( dbfname )
     dbf.add_field("RAINFALL", dbflib.FTDouble, 8, 2)
     dbf.add_field("RAINHOUR", dbflib.FTDouble, 8, 2)
     dbf.add_field("RAINPEAK", dbflib.FTDouble, 8, 2)
     
-    rs = mydb.query("select hrap_i, rainfall /25.4 as rain, \
-	peak_15min /25.4 * 4 as mrain, hr_cnt / 4.0 as hours from \
-	monthly_rainfall_%s  WHERE valid = '%s' \
-        ORDER by hrap_i ASC" % (now.strftime("%Y"), \
-        now.strftime("%Y-%m-%d") ) ).dictresult()
+    wcursor.execute("""select hrap_i, rainfall /25.4 as rain, 
+	peak_15min /25.4 * 4 as mrain, hr_cnt / 4.0 as hours from 
+	monthly_rainfall_%s  WHERE valid = '%s' 
+        ORDER by hrap_i ASC""" % (now.strftime("%Y"), 
+        now.strftime("%Y-%m-%d") ) )
 
     hrap = ohrap
-    for i in range(len(rs)):
-        #print rs[i]
-        hrap[ int(rs[i]['hrap_i']) ]= {'rain': float(rs[i]['rain']), \
-           'hours': float(rs[i]['hours']), 'mrain': float(rs[i]['mrain']) }
+    for row in wcursor:
+        hrap[ row[0] ]= {'rain': row[1], 
+           'hours': row[3], 'mrain': row[2] }
 
     for i in range(len(hrapi)):
         key = hrapi[i]
-        dbf.write_record(i, (hrap[key]['rain'], hrap[key]['hours'],\
-		hrap[key]['mrain'] ) )
+        dbf.write_record(i, (hrap[key]['rain'], hrap[key]['hours'],
+                             hrap[key]['mrain'] ) )
 
     del dbf
     outdir = "/mesonet/wepp/data/rainfall/shape/monthly/%s" % (now.year,)
     if not os.path.isdir(outdir):
         os.makedirs(outdir)
     shutil.copy(dbfname+".dbf", outdir)
-    shutil.copy("static/hrap_point_4326.shp", "monthlyrain/%s_rain.shp" % (now.strftime("%Y%m"), ) )
-    shutil.copy("static/hrap_point_4326.shx", "monthlyrain/%s_rain.shx" % (now.strftime("%Y%m"), ) )
-    shutil.copy("static/hrap_point_4326.prj", "monthlyrain/%s_rain.prj" % (now.strftime("%Y%m"), ) )
+    os.unlink("%s.dbf" % (dbfname,))
 
-    now += interval
+    now += datetime.timedelta(days=31)
 
