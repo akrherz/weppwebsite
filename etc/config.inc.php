d6e02f58a27f50fbabfeada3c8de03d7cc891767
akrherz <akrherz@iastate.edu>
2014-08-08 07:48:43 -0500

    remove config file from git

--- etc/config.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-date_default_timezone_set('America/Chicago');
-$_BASE = '/home/akrherz/projects/weppwebsite';
-$_WEBBASE = 'http://wepp.local';
-$weppdb = 'dbname=wepp host=iemdb user=nobody';
-
-$EDITOR = 0;
-define("TILECACHE", "http://iem.local");
-?>
