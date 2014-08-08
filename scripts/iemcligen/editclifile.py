59c3739e5aeffbbab772ee900c5cd2121cef107c
akrherz <akrherz@iastate.edu>
2014-08-08 09:01:45 -0500

    use write locks when writing and error when can't find date

--- scripts/iemcligen/editclifile.py
+++ scripts/iemcligen/editclifile.py
@@ -1,7 +1,7 @@
-# Something to edit Cli Files, not necessarily create them!
-
+""" Something to edit Cli Files, not necessarily create them!
+"""
 import datetime
-import sys
+import fcntl
 
 class editclifile:
 
@@ -11,21 +11,29 @@ class editclifile:
 
     def editDay(self, ts, cliRecord):
         pos0 = self.data.find("%s" % (ts.strftime("%-d\t%-m\t%Y"),) )
-        pos1 = self.data.find("%s" % \
-   ( (ts + datetime.timedelta(days=+1)).strftime("%-d\t%-m\t%Y"),) )
-
+        pos1 = self.data.find("%s" % ( 
+                (ts + datetime.timedelta(days=+1)).strftime("%-d\t%-m\t%Y"),) )
+        if pos0 == -1 or pos1 == -1:
+            raise Exception("Failed to find date: %s in filename: %s" % (
+                                                    ts.strftime("%b %d %Y"),
+                                                    self.filename,))
         self.data = self.data[:pos0] + str(cliRecord) + self.data[pos1:]
 
 
     def write(self):
         o = open(self.filename, 'w')
+        fcntl.lockf(o, fcntl.LOCK_EX, fcntl.LOCK_NB)
         o.write( self.data )
         o.close()
 
     def editDaySavePrecip(self, ts, cliRecord):
         pos0 = self.data.find("%s" % (ts.strftime("%-d\t%-m\t%Y"),) )
-        pos1 = self.data.find("%s" % \
-                              ( (ts + datetime.timedelta(days=+1)).strftime("%-d\t%-m\t%Y"),) )
+        pos1 = self.data.find("%s" % (
+                (ts + datetime.timedelta(days=+1)).strftime("%-d\t%-m\t%Y"),) )
+        if pos0 == -1 or pos1 == -1:
+            raise Exception("Failed to find date: %s in filename: %s" % (
+                                                    ts.strftime("%b %d %Y"),
+                                                    self.filename,))
         ex = (self.data[pos0:pos1]).find("\n")
         s = self.data[pos0+ex:pos1]
         if (len(s) > 3):
