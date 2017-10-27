1e078e7f3f882d5f8b6dbb5b2ad369b6d81bc4ff
akrherz <akrherz@iastate.edu>
2017-10-27 10:30:59 -0500

    pylint and run for 2017

--- scripts/util/merge_raindbf.py
+++ scripts/util/merge_raindbf.py
@@ -1,36 +1,46 @@
 """Combine the daily DBF files into a merged shapefile
 Note that we can only have less than 255 columns in a dbf file
 """
-import dbf
+from __future__ import print_function
 import datetime
+
+# note that dbf is a laptop local python thing for now
+import dbf
 import numpy as np
 
-sts = datetime.date(2016, 1, 1)
-ets = datetime.date(2016, 7, 11)
-interval = datetime.timedelta(days=1)
-now = sts
 
-precip = np.zeros((23182, 192))
+def main():
+    """Go Main Go"""
+    sts = datetime.date(2017, 3, 1)
+    ets = datetime.date(2017, 9, 30)
+    interval = datetime.timedelta(days=1)
+    now = sts
 
-res = []
-jday = 0
-while now < ets:
-    print now
-    fn = now.strftime("%m/%Y%m%d_rain.dbf")
-    d = dbf.Table(fn)
-    d.open()
-    i = 0
-    for record in d:
-        precip[i, jday] = record.rainfall
-        i += 1
-    res.append(now.strftime("%b%d") + " N(5,2)")
-    now += interval
-    jday += 1
+    precip = np.zeros((23182, int((ets - sts).days) + 1))
 
-comp = dbf.Table('combined', ";".join(res))
-comp.open()
-for i in range(23182):
-    ar = tuple(precip[i, :])
-    comp.append(ar)
+    res = []
+    jday = 0
+    while now <= ets:
+        print(now)
+        fn = now.strftime("%m/%Y%m%d_rain.dbf")
+        table = dbf.Table(fn)
+        table.open()
+        i = 0
+        for record in table:
+            precip[i, jday] = record.rainfall
+            i += 1
+        res.append(now.strftime("%b%d") + " N(5,2)")
+        now += interval
+        jday += 1
 
-comp.close()
+    comp = dbf.Table('combined', ";".join(res))
+    comp.open()
+    for i in range(23182):
+        ar = tuple(precip[i, :])
+        comp.append(ar)
+
+    comp.close()
+
+
+if __name__ == '__main__':
+    main()
