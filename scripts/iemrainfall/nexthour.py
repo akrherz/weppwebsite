15a63830651fad0e52ba9f9a122d8154b410fd1a
akrherz <akrherz@iastate.edu>
2019-08-27 14:33:28 -0500

    py3k

--- scripts/iemrainfall/nexthour.py
+++ scripts/iemrainfall/nexthour.py
@@ -1,13 +1,14 @@
-
-import mx.DateTime, sys
+"""Make life easier?"""
+from __future__ import print_function
+import datetime
+import sys
 
 yyyy = int(sys.argv[1])
 mm = int(sys.argv[2])
 dd = int(sys.argv[3])
 hr = int(sys.argv[4])
 
-ts = mx.DateTime.DateTime(yyyy,mm,dd,hr)
-t = ts + mx.DateTime.RelativeDateTime(hours=+1)
+ts = datetime.datetime(yyyy, mm, dd, hr)
+t = ts + datetime.timedelta(hours=+1)
 
-print t.strftime("%Y%m%d%H")
-
+print(t.strftime("%Y%m%d%H"))
