565e2919295b752e2fdaa255b5944e960a1e4c7e
akrherz <akrherz@iastate.edu>
2022-05-26 10:11:29 -0500

    update black

--- scripts/iemcligen/fe.py
+++ scripts/iemcligen/fe.py
@@ -1,20 +1,8 @@
-#!/usr/bin/env python
+import os
 
-import mx.DateTime, os, time
+from tqdm import tqdm
+import pandas as pd
 
-s = mx.DateTime.DateTime(2012,2, 1)
-e = mx.DateTime.DateTime(2012,8, 11)
-interval = mx.DateTime.RelativeDateTime(days=+1)
-
-now =s
-while (now<e):
-  #cmd = "./grids2shp.py %s" % (now.strftime("%Y %m %d"),)
-  #if ((now+interval).day == 1):
-  #  cmd = "./grids2shp.py %s 1" % (now.strftime("%Y %m %d"),)
-  cmd = "./build.py %s" % (now.strftime("%Y %m %d"),)
-  os.system(cmd)
-  #cmd = "psql -f rainfall.sql -h iemdb wepp"
-  #os.system(cmd)
-  print now
-  #time.sleep(60)
-  now += interval
+for date in tqdm(pd.date_range("2019/09/03", "2020/10/12")):
+    cmd = date.strftime("python grids2shp.py %Y %m %d")
+    os.system(cmd)
